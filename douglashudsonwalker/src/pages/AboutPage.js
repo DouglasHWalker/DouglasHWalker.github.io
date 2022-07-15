815042ac6e958eb64d2db2ce82c06c40dda723b8
Douglas Hudson Walker <douglas.hudsonwalker@gmail.com>
2022-07-15 22:43:51 +0930

    About me

--- douglashudsonwalker/src/pages/AboutPage.js
+++ douglashudsonwalker/src/pages/AboutPage.js
@@ -2,14 +2,48 @@ import React from 'react';
 
 const AboutPage = () => (
     <>
-        <h1>About Me</h1>
-        <p>
-            Hi, my name is Douglas and I'm a full-stack engineer and general automator with a passion for solving difficult problems.
-            I 
-        </p>
-        <p>
-            Recently, I have been helping to optimise humanitarian crisis response for the Defence Science and Technology Group.
-        </p>
+        <div className='about'>
+            <h1>About Me</h1>
+            <p>
+                I'm a full-stack engineer with a passion for solving complex problems. I've helped optimise humanitarian crisis response, build financial algorithms, predict Premier League results, automate online BlackJack, and build personalised online education applications.
+            </p>
+            <p>
+                I'd like to use cutting-edge technology to solve the world’s most challenging problems. By building practical tools we can digitally transform the world and extend humanties capabilities for the better!
+            </p>
+            <p>
+                I have experience working with web, mobile, and desktop development stacks aswell as complex modelling languages, artificial intelligence, and cloud computing platforms.
+                Here are some of the technolgies I have been working with recently:
+            </p>
+            <ul className='about-tech' >
+                <li>C++</li>
+                <li>Java</li>
+                <li>Python</li>
+                <li>React Native</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>Kotlin</li>
+                <li>Swift</li>
+                <li>ASP.NET MVC</li>
+                <li>AWS</li>
+                <li>Azure</li>
+                <li>TypeScript</li>
+                <li>CI/CD</li>
+                <li>C#</li>
+                <li>MVVC</li>
+                <li>Design Patterns</li>
+                <li>SCRUM</li>
+                <li>Algorithms</li>
+                <li>Optimisation</li>
+                <li>Data Structures</li>
+                <li>NoSQL</li>
+                <li>SQL</li>
+                <li>MongoDB</li>
+
+            </ul>
+        </div>
     </>
 );
 
--- douglashudsonwalker/src/pages/HomePage.js
+++ douglashudsonwalker/src/pages/HomePage.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ProjectList from '../components/ProjectList'
+import AboutPage from './AboutPage';
 import projectContent from './project-content'
 
 const HomePage = () => (
     <>
         <div className='title'>
-            <h1 className='hero-name'>Hi, my name is Douglas</h1>
-            <h2 className='tagline'>I solve problems using code.</h2> {/* A series of flipping taglines which describe all the things I do. */}
-            <p className='intro'>I'm a software engineer with a passion for innovative technology. I enjoy building practical solutions to complex problems.</p>
+            <h1 className='hero-name'>Hi, I'm Douglas</h1>
+            <h2 className='tagline'>I find solutions.</h2> {/* A series of flipping taglines which describe all the things I do. */}
+            <p className='intro'>I'm an software engineer with a passion for problem solving and using innovative technology to extend user capabilties. </p>
             {/* <Link className='call-to-action-link' to="/about">See my Work!</Link> */}
-            <div class="down-arrow"></div>
+            <div class="down-arrow"><p>Scroll for more.</p></div>
         </div>
+        <AboutPage />
         <div className='featured'>
             <h2 className='highlight'>Featured Projects</h2>
             <ProjectList projects={projectContent} />
