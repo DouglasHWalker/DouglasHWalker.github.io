455040fbb9ed27fdfb9a1e47042f058097e758a4
Douglas Hudson Walker <douglas.hudsonwalker@gmail.com>
2022-07-24 15:21:25 +0930

    Contact and Theme

--- douglashudsonwalker/src/pages/project-content.js
+++ douglashudsonwalker/src/pages/project-content.js
@@ -13,11 +13,33 @@ const projects = [
             'Performed large user-studies and collated results for use in research publications.',
         ],
         image: require("../images/jodia.png"),
-        technologies: ["Practical use of: ", "JavaScript", "Python", "D3", "Reinforcement Learning", "Gym","Unity", "Design Patterns", "Data Structures", "LaTeX", "Technical Writing", "User Studies"]
+        technologies: ["JavaScript", "Python", "D3", "Design Patterns", "Reinforcement Learning", "Gym", "Unity", "Data Structures", "LaTeX", "Technical Writing", "Conducting User Studies"]
+    },
+    {
+        name: 'bankstudios',
+        title: 'Automate Financial Planning',
+        summary: [
+            'Integer maximus tortor vel mauris bibendum, ac iaculis dolor ullamcorper.Curabitur dapibus feugiat venenatis.Morbi ac enim sed sem varius sodales tincidunt non orci.',
+            'Integer venenatis viverra leo, ac malesuada dolor finibus ut.Praesent quis nunc id elit vehicula blandit.Cras aliquam venenatis nisi, id consectetur augue dapibus sit amet.',
+        ],
+        content: [
+            'Predicting the weather is difficult when the lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta dignissim mauris, non dapibus justo. Cras a ultrices nisi, quis auctor justo.',
+            'Integer maximus tortor vel mauris bibendum, ac iaculis dolor ullamcorper.Curabitur dapibus feugiat venenatis.Morbi ac enim sed sem varius sodales tincidunt non orci.',
+            'Integer venenatis viverra leo, ac malesuada dolor finibus ut.Praesent quis nunc id elit vehicula blandit.Cras aliquam venenatis nisi, id consectetur augue dapibus sit amet.',
+            'Morbi quis massa libero.Interdum et malesuada fames ac ante ipsum primis in faucibus.Pellentesque id felis nibh.Sed a neque posuere, interdum neque sed, consectetur leo.',
+            'Nunc convallis elementum felis, nec volutpat risus aliquam a.Cras consequat orci ac pharetra aliquet.Proin iaculis quam orci, ac volutpat velit pretium eu.Praesent sed consequat purus.',
+            'Curabitur gravida, libero pellentesque dictum finibus, ipsum erat efficitur nunc, quis ullamcorper augue elit et felis.Maecenas quis enim dolor.',
+            'Maecenas pellentesque sapien sit amet facilisis dictum.Donec posuere pharetra sapien ac porta.Donec imperdiet ornare turpis.In lobortis tempor quam ut sollicitudin.',
+            'Cras maximus tincidunt lacus, in gravida magna auctor a.Suspendisse aliquam, augue in efficitur rhoncus, orci mi auctor magna, in venenatis quam ante eget ex.',
+            'Ut hendrerit purus ac tortor vestibulum, facilisis pharetra felis ultricies.Sed sit amet nulla condimentum, ultricies erat vitae, semper ex.',
+            'Morbi rutrum laoreet purus, vitae venenatis est efficitur eu.Pellentesque pharetra bibendum rhoncus.Sed tempus dignissim elementum.'
+        ],
+        image: require("../images/predictions.png"),
+        technologies: ["C++", "React Native", "Node.js", "Python", "Swift"]
     },
     {
         name: 'lifetime',
-        title: 'Customer Pomodoro Application for iOS.',
+        title: 'Decipher News Veracity.',
         summary: [
             'Integer maximus tortor vel mauris bibendum, ac iaculis dolor ullamcorper.Curabitur dapibus feugiat venenatis.Morbi ac enim sed sem varius sodales tincidunt non orci.',
             'Integer venenatis viverra leo, ac malesuada dolor finibus ut.Praesent quis nunc id elit vehicula blandit.Cras aliquam venenatis nisi, id consectetur augue dapibus sit amet.',
@@ -35,11 +57,11 @@ const projects = [
             'Morbi rutrum laoreet purus, vitae venenatis est efficitur eu.Pellentesque pharetra bibendum rhoncus.Sed tempus dignissim elementum.'
         ],
         image: require("../images/Dalle.jpeg"),
-        technologies: ["C++", "React Native", "Node.js", "Python", "Swift"]
+        technologies: ["Natural Language Processing", "Python", "BERT", "LaTeX", "Technical Writing"]
     },
     {
         name: 'bankstudios',
-        title: 'The Bankstudios website',
+        title: 'Personalise Online Education',
         summary: [
             'Integer maximus tortor vel mauris bibendum, ac iaculis dolor ullamcorper.Curabitur dapibus feugiat venenatis.Morbi ac enim sed sem varius sodales tincidunt non orci.',
             'Integer venenatis viverra leo, ac malesuada dolor finibus ut.Praesent quis nunc id elit vehicula blandit.Cras aliquam venenatis nisi, id consectetur augue dapibus sit amet.',
